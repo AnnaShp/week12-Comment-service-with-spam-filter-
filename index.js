bbcb24afca44add695ae58000943c68d8a670741
AnnaShp <nyutka_slutskaya@mail.ru>
2023-05-27 01:03:26 +0300

    working spam-filter form

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-'use strict';
+"use strict";
+
 // обьявление переменных
 let userName;
 let userLink;
@@ -11,7 +12,7 @@ function sendName() {
     let firstLetter = userName[0].toUpperCase();
     const user = userName.slice(1).toLowerCase();
     userName = firstLetter + user;
-    return userName;
+    document.getElementById("resultName").innerText = userName;
 }
 
 //функция "забирает" высланную ссылку
@@ -24,39 +25,14 @@ function getText() {
     userText = document.getElementById('user_text').value;
 }
 
-//обьявление спам-слов и замена их на ***
-let str = getText();
-let spam1;
-let spam2;
-let newStr;
-
-function checkSpam() {
-    spam1 = /viagra/gi;
-    spam2 = /xxx/gi;
-    newStr = str.replaceAll((spam1 | spam2), '***');
-    return newStr;
+// обьявление спам-слов для замены
+function checkSpam () {
+    let newStr = userText.replace(/(xxx|viagra)/gi, "***");
+    document.getElementById("resultText").innerText = newStr;
 }
-
-// userText = checkSpam(); - нужно?
 
 function sendAll() {
     document.getElementById('resultLink').src = userLink;
-    document.getElementById('resultName').innerText = userName;
-    document.getElementById('resultText').innerText = userText;
+    sendName();
+    checkSpam ();
 }
-
-
-
-
-
-
-
-
-
-
-/*document.querySelector('.button').addEventListener('click', function sendAll() {
-    let result = document.getElementById('result');
-    let name = str.slice(0);
-    console.log(name);
-
-});*/
