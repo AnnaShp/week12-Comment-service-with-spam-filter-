ef10071590635e78f4519af00f4cc3b1ad25388d
AnnaShp <nyutka_slutskaya@mail.ru>
2023-05-29 16:19:24 +0300

    little changes to clean up the code

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 "use strict";
 
 // обьявление переменных
-let userName;
-let userLink;
+let userName = document.getElementById('user_name').value;
+let userLink= document.getElementById('user_link').value;
 let userText;
 
 //функция возвращения исправленного имени в чат
 function sendName() {
-    userName = document.getElementById('user_name').value;
-    userName = userName.replaceAll(' ', '');
     let firstLetter = userName[0].toUpperCase();
     const user = userName.slice(1).toLowerCase();
     userName = firstLetter + user;
     document.getElementById("resultName").innerText = userName;
 }
 
-//функция "забирает" высланную ссылку
-function sendLink() {
-    userLink = document.getElementById('user_link').value;
-}
-
-//функция "забирает" высланный текст
-function getText() {
-    userText = document.getElementById('user_text').value;
+function getText (){
+    userText = document.querySelector('#user_text').value;
 }
 
 // обьявление спам-слов для замены
-function checkSpam () {
+function checkSpam() {
     let newStr = userText.replace(/(xxx|viagra)/gi, "***");
     document.getElementById("resultText").innerText = newStr;
 }
@@ -34,5 +26,5 @@ function checkSpam () {
 function sendAll() {
     document.getElementById('resultLink').src = userLink;
     sendName();
-    checkSpam ();
+    checkSpam();
 }
